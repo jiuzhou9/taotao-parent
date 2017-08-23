9acb30c2d588775efdb230ebf7c669167f4b1605
jiuzhou9 <835540436@qq.com>
2017-08-23 21:52:08 +0800

    1.页面的跳转 2.实现条件查询分页

---
+++ taotao-manager-api/src/main/java/com/taotao/common/PageDetail.java
@@ -0,0 +1,61 @@
+package com.taotao.common;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PageDetail<T> implements Serializable {
+    private int pageNum;
+    private int pageSize;
+    private int size;
+    private long total;
+    private int pages;
+    private List<T> list;
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
